c7f67e0421f9c3a142cd2c30563e1fc37e209c95
Matteo Barbieri <matteo@barbieri.xyz>
2022-07-04 15:17:26 +0200

    Add common and another library

---
+++ library-one/src/main/java/libraryone/LibraryOne.java
@@ -0,0 +1,11 @@
+package libraryone;
+
+import commons.CommonModel;
+
+public class LibraryOne {
+
+    public static void main(String[] args) {
+        new CommonModel();
+        System.out.println("library-one");
+    }
+}
