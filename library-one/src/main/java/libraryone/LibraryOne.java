1a86ee35785244d502c55d4b469f8cc4b66bab41
Matteo Barbieri <matteo@barbieri.xyz>
2022-07-05 09:36:44 +0200

    Test change in library one

--- library-one/src/main/java/libraryone/LibraryOne.java
+++ library-one/src/main/java/libraryone/LibraryOne.java
@@ -4,6 +4,8 @@ import commons.CommonModel;
 
 public class LibraryOne {
 
+    private static final int i = 0;
+
     public static void main(String[] args) {
         new CommonModel();
         System.out.println("library-one");
